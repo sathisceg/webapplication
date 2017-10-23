f9712055eda60ffda583061eb0eb0f17df783c3e
sathis92 <sathiscode2@gmail.com>
2017-10-23 01:01:04 +0000

    changes to share search register

--- controller/cabController.js
+++ controller/cabController.js
@@ -1,3 +1,36 @@
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+mongoose.connect('mongodb://localhost/mydb',{ useMongoClient: true });
+
+mongoose.connection.once('open',function(){
+
+  console.log("connected mongo db success");
+
+}).on('error',function(){
+
+  console.log("connection to  mongo db is failed");
+
+});
+
+
+const Schema = mongoose.Schema;
+
+const RegisterUserSchema = new Schema({
+
+  username:String,
+  password:String,
+  email:String
+
+});
+
+
+const RegisterUser = mongoose.model('registerusercollections',RegisterUserSchema);
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
 
@@ -12,21 +45,63 @@ app.get('/login',function(req,res){
 });
 
 
-app.get('/register',function(req,res){
+app.get('/register',function(req,res,next){
 
   //  res.end("register page");
+
+    console.log("register");
+
     res.render('register');
 });
 
 
 
-app.get('/generateotp',function(req,res){
+app.post('/register_user_database',urlencodedParser,function(req,res){
 
-  //console.log("generateotp");
 
-  //res.render('register');
+      console.log("register_user_database");
+      console.log(req.body);
+
+
+      const user = new RegisterUser({
+        username:req.body.username,
+        password:req.body.password,
+        email:req.body.email
+      });
+
+      user.save().then(function(){
+            console.log("file inserted to database");
+      });
+
+
+
+
+
+      // mongoose.connection.collections.registerusercollections.drop(function(){
+      //     console.log("collection deleted from database");
+      // });
+
+
+
 
 });
+
+// app.post('/register',urlencodedParser,function(req,res){
+//
+//   //  res.end("register page");
+//
+//     console.log("register post");
+//     res.redirect('/register');
+// });
+
+
+// app.get('/generateotp',function(req,res){
+//
+//   //console.log("generateotp");
+//
+//   //res.render('register');
+//
+// });
 
 
 
